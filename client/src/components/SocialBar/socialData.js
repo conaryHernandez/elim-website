964c01c9f523230e980111d552a4e7e1d38c34e2
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-02-09 20:14:17 -0600

    Code HTML and CSS for SocialBar Component (#1)
    adding social bar component
    addinf social networks icons

---
+++ client/src/components/SocialBar/socialData.js
@@ -0,0 +1,19 @@
+const socialData = [
+  {
+    id: 'social-1',
+    name: 'facebook',
+    link: '#'
+  },
+  {
+    id: 'social-2',
+    name: 'twitter',
+    link: '#'
+  },
+  {
+    id: 'social-3',
+    name: 'instagram',
+    link: '#'
+  }
+];
+
+export default socialData;
