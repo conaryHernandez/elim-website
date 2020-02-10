43b1c39f590590d50b1e1c344329da1d78bcf428
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-02-09 21:44:45 -0600

    Code HTML and CSS for Footer
    adding footer data and final markup

--- client/src/containers/App/components/Footer/Footer.js
+++ client/src/containers/App/components/Footer/Footer.js
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Row, Col } from 'antd';
 
 import ListItems from '../../../../components/UI/ListItems/ListItems';
-import footerData from './footerData';
+import SocialBar from '../../../../components/SocialBar/SocialBar';
 import classes from './Footer.module.scss';
 
-console.log('footerData', footerData);
-
 const Footer = () => {
+  const [footerData, setFooterData] = useState({});
+
+  useEffect(() => {
+    async function getFooterData() {
+      try {
+        const response = await axios.get('data/footerData.json');
+
+        setFooterData(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getFooterData();
+  }, []);
+
   return (
     <div className={classes.Footer}>
-      <h2 className={`${classes.FooterTitle} txt-center`}>
-        Mision Cristiana Elim
-      </h2>
-
-      <ListItems
-        className={classes.FooterSpecialList}
-        data={footerData?.specialLinks}
-      />
-
-      <Row>
-        {footerData?.sections?.map(item => (
-          <Col key={`list-${item.title}`} xs={24} md={12} lg={6}>
-            <ListItems
-              className={classes.FooterList}
-              title={item.title}
-              data={item.items}
+      <div className="container">
+        <Row>
+          <Col xs={24}>
+            <SocialBar
+              className={classes.SocialBar}
+              networks={footerData.socialNetworks}
             />
           </Col>
-        ))}
-      </Row>
+        </Row>
+
+        <h2 className={`${classes.FooterTitle} txt-center`}>
+          Misión Cristiana Elim
+          <span className="display-block">
+            <i className="icon-location" />
+          </span>
+        </h2>
+
+        <div className={classes.Location}>
+          <span className={`${classes.Location} txt-center display-block`}>
+            {footerData?.location?.address}
+          </span>
+        </div>
+
+        <ListItems
+          className={classes.FooterSpecialList}
+          data={footerData?.specialLinks}
+        />
+
+        <Row>
+          {footerData?.sections?.map(item => (
+            <Col key={`list-${item.title}`} xs={24} md={12} lg={6}>
+              <ListItems
+                className={classes.FooterList}
+                title={item.title}
+                data={item.items}
+              />
+            </Col>
+          ))}
+        </Row>
+      </div>
+      <div className={classes.FooterLegend}>
+        <p>
+          © Copyright 2017 — {new Date().getFullYear()} Misión Cristiana Elim.
+          Todos los derechos reservados.
+        </p>
+      </div>
     </div>
   );
 };
