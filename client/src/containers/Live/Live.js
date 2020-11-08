32f8200456063e6d059a887e47f222183544eb83
conary <conary@hotmail.es>
2020-11-07 23:18:41 -0600

    strapi integration

--- client/src/containers/Live/Live.js
+++ client/src/containers/Live/Live.js
@@ -97,7 +97,7 @@ export default function Live() {
           <iframe
             width="100%"
             height="100%"
-            src="https://boxcast.tv/view-embed/test-hbhvfuwbq9t0e4ygpmr1?showTitle=0&showDescription=0&showHighlights=0&showRelated=0&showCountdown=1&market=house-of-worship&showDocuments=0&showIndex=0&showDonations=0"
+            src={pageData.videoId}
             frameBorder="0"
             scrolling="auto"
             allowfullscreen="true"
